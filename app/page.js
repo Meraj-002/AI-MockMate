9b82cf1b7478607c0f358ed620bd489fdddd8b11
Meraj Alam <techymeraj@gmail.com>
2025-08-05 09:55:32 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,50 +1,79 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { SignUp } from '@clerk/nextjs'
+// import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+// import { SignUp } from '@clerk/nextjs'
 
-export default function Home() {
-  return (
-    <main className="w-full flex">
-              <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
-                  <div className="relative z-10 w-full max-w-md">
-                      <img src='/banlogow.png' width={250} />
-                      <div className=" mt-16 space-y-3">
-                          <h3 className="text-white text-3xl font-bold">Welcome to AI Mock Mate</h3>
-                          <p className="text-gray-300">
-                          Generate personalized interview questions based on your job role and experience.
-                              </p>
-                          <div className="flex items-center -space-x-2 overflow-hidden">
-                              <img src="https://randomuser.me/api/portraits/women/79.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                              <img src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                              <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f" className="w-10 h-10 rounded-full border-2 border-white" />
-                              <img src="https://randomuser.me/api/portraits/men/86.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
-                              <img src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e" className="w-10 h-10 rounded-full border-2 border-white" />
-                              <p className="text-sm text-gray-400 font-medium translate-x-5">
-                                  Join 5.000+ users
-                              </p>
-                          </div>
-                      </div>
-                  </div>
-                  <div
-                      className="absolute inset-0 my-auto h-[500px]"
-                      style={{
-                          background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)", filter: "blur(118px)"
-                      }}
-                  >
+// export default function Home() {
+//   return (
+//     <main className="w-full flex">
+//               <div className="relative flex-1 hidden items-center justify-center h-screen bg-gray-900 lg:flex">
+//                   <div className="relative z-10 w-full max-w-md">
+//                       <img src='/banlogow.png' width={250} />
+//                       <div className=" mt-16 space-y-3">
+//                           <h3 className="text-white text-3xl font-bold">Welcome to AI Mock Mate</h3>
+//                           <p className="text-gray-300">
+//                           Generate personalized interview questions based on your job role and experience.
+//                               </p>
+//                           <div className="flex items-center -space-x-2 overflow-hidden">
+//                               <img src="https://randomuser.me/api/portraits/women/79.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
+//                               <img src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
+//                               <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f" className="w-10 h-10 rounded-full border-2 border-white" />
+//                               <img src="https://randomuser.me/api/portraits/men/86.jpg" className="w-10 h-10 rounded-full border-2 border-white" />
+//                               <img src="https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e" className="w-10 h-10 rounded-full border-2 border-white" />
+//                               <p className="text-sm text-gray-400 font-medium translate-x-5">
+//                                   Join 5.000+ users
+//                               </p>
+//                           </div>
+//                       </div>
+//                   </div>
+//                   <div
+//                       className="absolute inset-0 my-auto h-[500px]"
+//                       style={{
+//                           background: "linear-gradient(152.92deg, rgba(192, 132, 252, 0.2) 4.54%, rgba(232, 121, 249, 0.26) 34.2%, rgba(192, 132, 252, 0.1) 77.55%)", filter: "blur(118px)"
+//                       }}
+//                   >
   
-                  </div>
-              </div>
-              <div className="flex-1 flex items-center justify-center h-screen">
-                  <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
-                      <div className="">
-                          <img src="/banlogo.png" width={250} className="lg:hidden" />
+//                   </div>
+//               </div>
+//               <div className="flex-1 flex items-center justify-center h-screen">
+//                   <div className="w-full max-w-md space-y-8 px-4 bg-white text-gray-600 sm:px-0">
+//                       <div className="">
+//                           <img src="/banlogo.png" width={200} className="lg:hidden" />
                           
-                      </div>
+//                       </div>
                       
                       
-                      <SignUp />
-                  </div>
-              </div>
-          </main>
-  );
+//                       <SignUp />
+//                   </div>
+//               </div>
+//           </main>
+//   );
+// }
+
+
+import React from 'react'
+import { UserButton } from '@clerk/nextjs'
+import AddNewInterview from './dashboard/_components/AddNewInterview'
+import InterviewList from './dashboard/_components/InterviewList'
+import Header from './dashboard/_components/Header'
+
+function Home() {
+  return (
+    
+    <div>
+      <Header />
+<div className='p-10'>
+      <h2 className='font-bold text-2xl'>Dashboard</h2>
+      <h2 className='text-gray-500'>Create and Start your AI Mockup Interview</h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
+        <AddNewInterview />
+      </div>
+
+      {/* Previous Interview List */}
+      <InterviewList />
+    </div>
+    </div>
+  )
 }
+
+export default Home
